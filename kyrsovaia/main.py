f071065eb5f364ce46c67eb2c8535b4156d5b325
HARB1I <156460883+HARB1I@users.noreply.github.com>
2024-12-24 20:34:32 +0400

    Add files via upload

--- kyrsovaia/main.py
+++ kyrsovaia/main.py
@@ -1,6 +1,8 @@
+
 import tkinter as tk
 import random
 import os
+import json
 
 BOARD_SIZE = 8
 SQUARE_SIZE = 80
@@ -8,6 +10,7 @@ selected_square = None
 board = None
 turn = 'white'
 game_over = False
+user_data = {}  # Словарь для хранения данных пользователей
 
 def create_board():
     return [['0' for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
@@ -90,7 +93,7 @@ def is_king_captured(team):
                 return False
     return True
 
-def on_square_click(event):
+def on_click(event):
     global selected_square, turn, game_over
     if game_over:
         return  
@@ -110,14 +113,17 @@ def on_square_click(event):
                 set_piece(row, col, piece)
             set_piece(old_row, old_col, '0')
             turn = 'black' if turn == 'white' else 'white'
+            winner_label.config(text=turn)
             if is_king_captured(1):
                 game_over = True
                 winner_label.config(text="Черные победили!")
                 restart_button.pack()
+                resize_and_center(640,690)
             elif is_king_captured(2):
                 game_over = True
                 winner_label.config(text="Белые победили!")
                 restart_button.pack()
+                resize_and_center(640,690)
         else:
              selected_square = None 
         selected_square = None
@@ -132,6 +138,8 @@ def on_square_click(event):
 
 def draw(canvas, board):
     canvas.delete("all")
+    if game_over == False:
+        winner_label.config(text=turn)
     possible_moves = []
     if selected_square:
         row, col = selected_square
@@ -198,19 +206,117 @@ def restart_game():
     winner_label.config(text="")
     restart_button.pack_forget()
     draw(canvas, board)
+    resize_and_center(640,663)
+
+def resize_and_center(window_width, window_height):
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x_cordinate = int((screen_width / 2) - (window_width / 2))
+    y_cordinate = int((screen_height / 2) - (window_height / 2))
+    root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+
+def load_user_data():
+    """Загружает данные пользователей из файла users.json."""
+    global user_data
+    try:
+        with open("users.json", "r") as f:
+            user_data = json.load(f)
+    except FileNotFoundError:
+        pass  # Файл еще не существует
+
+def save_user_data():
+    """Сохраняет данные пользователей в файл users.json."""
+    try:
+        with open("users.json", "w") as f:
+            json.dump(user_data, f, indent=4)
+    except IOError as e:
+        print(f"Ошибка записи в файл: {e}")
+
+
+def register():
+    """Регистрирует нового пользователя."""
+    username = username_entry.get()
+    password = password_entry.get()
+    if username in user_data:
+        register_label.config(text="Пользователь уже существует!")
+    else:
+        user_data[username] = {"password": password}
+        save_user_data()
+        register_label.config(text="Регистрация прошла успешно!")
+
+def login():
+    """Авторизует пользователя."""
+    username = username_entry.get()
+    password = password_entry.get()
+    if username in user_data and user_data[username]["password"] == password:
+        login_label.config(text="Вход выполнен успешно!")
+        login_frame.pack_forget()
+        canvas.pack()
+        winner_label.pack()
+        restart_button.pack()
+        start_game()
+        
+
+
+    else:
+        login_label.config(text="Неверный логин или пароль!")
+
+
+def start_game():
+    global board, turn, game_over, selected_square
+    board = init_board()
+    turn = 'white'
+    game_over = False
+    selected_square = None
+    winner_label.config(text="")
+    restart_button.pack_forget()
+    draw(canvas, board)
+    resize_and_center(640,663)
+    
+
 
 root = tk.Tk()
+root.resizable(width=False, height=False)
 root.title("Chess")
 
 canvas = tk.Canvas(root, width=BOARD_SIZE * SQUARE_SIZE, height=BOARD_SIZE * SQUARE_SIZE)
-canvas.pack()
+
+resize_and_center(BOARD_SIZE * SQUARE_SIZE, BOARD_SIZE * SQUARE_SIZE + 50)
 
 winner_label = tk.Label(root, text="")
 winner_label.pack()
 
 restart_button = tk.Button(root, text="Restart Game", command=restart_game)
 
+
+# Окно регистрации/авторизации
+login_frame = tk.Frame(root)
+login_frame.pack()
+resize_and_center(250,120)
+username_label = tk.Label(login_frame, text="Имя пользователя:")
+username_label.grid(row=0, column=0)
+username_entry = tk.Entry(login_frame)
+username_entry.grid(row=0, column=1)
+
+password_label = tk.Label(login_frame, text="Пароль:")
+password_label.grid(row=1, column=0)
+password_entry = tk.Entry(login_frame, show="*")
+password_entry.grid(row=1, column=1)
+
+register_button = tk.Button(login_frame, text="Зарегистрироваться", command=register)
+register_button.grid(row=2, column=0, columnspan=2, sticky="w", pady=15)
+register_label = tk.Label(login_frame, text="")
+register_label.grid(row=3, column=0, columnspan=2)
+
+login_button = tk.Button(login_frame, text="Войти", command=login)
+login_button.grid(row=2, column=1, columnspan=2, sticky="e")
+login_label = tk.Label(login_frame, text="")
+login_label.grid(row=3, column=1, columnspan=2)
+
+load_user_data()  # Загрузка данных пользователей при запуске
+
 board = init_board()
-canvas.bind("<Button-1>", on_square_click)
-draw(canvas, board)
+canvas.bind("<Button-1>", on_click)
+
+
 root.mainloop()
